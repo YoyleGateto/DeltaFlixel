b522dfc4674a0790d5c994fc8ab4b0d0544c9b76
YoyleGateto <156556823+YoyleGateto@users.noreply.github.com>
2026-01-05 21:33:37 -0400

    same as the other one

--- source/deltaflixel/objects/DeltaEnemy.hx
+++ source/deltaflixel/objects/DeltaEnemy.hx
@@ -18,11 +18,8 @@ class DeltaEnemy extends FunkinSprite
 		acts = _acts;
 		frames = Paths.getFrames(_spritesheet);
 		scale.set(_scale,_scale);
-		for (anim in _animations) {
-			var autoIndices = [];
-			if (anim.endFrame != null) for (i in 0...(anim.endFrame + 1))
-				autoIndices.push(i);
-			addAnim(anim.name, anim.prefix, anim.fps, anim.loop, false, anim.indices == null ? autoIndices : anim.indices);
+		for (anim in _animations) {
+			addAnim(anim.name, anim.prefix + "0", anim.fps, anim.loop, false, anim.indices == null ? [] : anim.indices);
 			if (anim.offset != null)
 				addOffset(anim.name, anim.offset[0], anim.offset[1]);
 			else
